e38ff2f602b33f1cc6456aee237c5a4f0418be31
Alejandro Vaz <alejandro.vaz.myt@gmail.com>
2026-07-23 01:31:18 +0200

    fix(stack-array): errors now implement debug

--- stack-array/errors.rs
+++ stack-array/errors.rs
@@ -1,18 +1,15 @@
 //^
-//^ HEAD
-//^
-
-
-//^
 //^ ERRORS
 //^
 
 //> ERRORS -> UNMATCHED CAPACITY
+#[derive(Debug)]
 pub struct UnmatchedCapacity<const EXPECTED: usize> {
     pub present: usize
 }
 
 //> ERRORS -> CAPACITY EXCEEDED
+#[derive(Debug)]
 pub struct CapacityExceeded<const N: usize> {
     pub expected: usize
 }
